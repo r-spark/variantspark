3deae1f9611a3aea1357c450a15423a16702a02b
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2019-05-08 14:53:09 -0300

    vs_connect documentation

---
+++ R/vs_connect.R
@@ -0,0 +1,21 @@
+#' Creating a variantspark connection
+#' 
+#' You need to create a variantspark connection to use this extension. To do this, 
+#' you pass as argument a spark connection that you can create 
+#' using \code{sparklyr::spark_connect()}. 
+#' 
+#' @param sc A spark connection.
+#' 
+#' @return A variantspark connection
+#' 
+#' @examples 
+#' \dontrun(
+#' library(sparklyr)
+#' sc <- spark_connect(master = "local")
+#' vsc <- vs_connect(sc)
+#' )
+#'
+#' @export
+vs_connect <- function(sc){
+  sparklyr::invoke_new(sc, "au.csiro.variantspark.api.VSContext", spark_session(sc))
+}
