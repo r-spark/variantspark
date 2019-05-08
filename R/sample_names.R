50f97d3b5d54185811aac5d2f19f31bdf6e0dc4b
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2019-05-08 14:52:03 -0300

    sample_names documentation

---
+++ R/sample_names.R
@@ -0,0 +1,37 @@
+#' Display sample names
+#' 
+#' This function display the first N variant names.
+#' 
+#' @param vcf_source An object with \code{VCFFeatureSource} class, usually the 
+#' output of the \code{vs_read_vcf()}.
+#' @param n_samples The number os samples to display.
+#' 
+#' @return spark_jobj, shell_jobj
+#' 
+#' @examples 
+#' \dontrun{
+#' library(sparklyr)
+#' 
+#' sc <- spark_connect(master = "local", version = "2.2.0")
+#''vsc <- vs_connect(sc)
+#' 
+#' hipster_vcf <- vs_read_vcf(vsc, "data/hipster.vcf.bz2")
+#' sample_names(hipster_vcf, 3)
+#' }
+#'
+#' @export
+sample_names <- function(vcf_source, n_samples = NULL){
+  names <- sparklyr::invoke(vcf_source, "sampleNames")
+  
+  if (!is.null(n_samples)){
+    names <- sparklyr::invoke(names, "take", as.integer(n_samples))  
+  }
+  names
+}
+
+
+
+
+
+
+
