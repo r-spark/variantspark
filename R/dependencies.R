030abe3fb977ef87728b5c7da69bd08fa9ee7390
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2019-05-16 10:53:07 -0300

    remove jars and include variantspark by maven

--- R/dependencies.R
+++ R/dependencies.R
@@ -1,33 +1,10 @@
 spark_dependencies <- function(spark_version, scala_version, ...) {
   sparklyr::spark_dependency(
-    jars = c(
-      system.file(
-        sprintf("java/hail.comparison-%s-%s.jar", spark_version, scala_version),
-        package = "hail.comparison"
-      ),
-      system.file(
-        sprintf("java/variant-spark_2.11-0.2.0-a1.jar"),
-        package = "hail.comparison"
-      ),
-      system.file(
-        sprintf("java/scala-csv_2.11-1.2.2.jar"),
-        package = "hail.comparison"
-        ),
-      system.file(
-        sprintf("java/htsjdk-2.14.0.jar"),
-        package = "hail.comparison"
-      ),
-      system.file(
-        sprintf("java/dsiutils-2.5.4.jar"),
-        package = "hail.comparison"
-      ),
-      system.file(
-        sprintf("java/fastutil-7.1.0.jar"),
-        package = "hail.comparison"
-      )     
-    ),
-    packages = c(
-    )
+    packages = c("au.csiro.aehrc.variant-spark:variant-spark_2.11:0.2.0-a1"),
+    repositories = c(
+      "https://mvnrepository.com/artifact/",
+      "http://central.maven.org/maven2/"
+                     )
   )
 }
 
