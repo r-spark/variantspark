8ce0256fed4a37fbfc9649b75c71fd8b56a6ccc0
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2019-05-16 11:36:06 -0300

    add spark_install()

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,10 @@
+Sys.setenv("R_TESTS" = "")
 library(testthat)
 library(variantspark)
+library(sparklyr)
+
+spark_install(version = "2.4.0")
+spark_install(version = "2.3.2")
+spark_install(version = "2.2.1")
 
 test_check("variantspark")
