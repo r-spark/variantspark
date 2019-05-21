d4ccaa7bc90e9cb778e7f949961045d853e03d85
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2019-05-21 20:12:49 -0300

    check spark versions installed before testthat

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,8 +3,10 @@ library(testthat)
 library(variantspark)
 library(sparklyr)
 
-spark_install(version = "2.4.0")
-spark_install(version = "2.3.2")
-spark_install(version = "2.2.1")
+spark_versions <- spark_available_versions()
+
+if (!any(spark_versions == "2.4")) spark_install(version = "2.4.0")
+if (!any(spark_versions == "2.3")) spark_install(version = "2.3.2")
+if (!any(spark_versions == "2.2")) spark_install(version = "2.2.1")
 
 test_check("variantspark")
