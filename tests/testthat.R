03db3df8b2ccf4af6f4bce5433cc5d40feaba1f5
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2019-05-21 20:26:33 -0300

    remove if statement

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,10 +3,8 @@ library(testthat)
 library(variantspark)
 library(sparklyr)
 
-spark_versions <- spark_available_versions()
-
-if (!any(spark_versions == "2.4")) spark_install(version = "2.4.0")
-if (!any(spark_versions == "2.3")) spark_install(version = "2.3.2")
-if (!any(spark_versions == "2.2")) spark_install(version = "2.2.1")
+spark_install(version = "2.4.0")
+spark_install(version = "2.3.2")
+spark_install(version = "2.2.1")
 
 test_check("variantspark")
