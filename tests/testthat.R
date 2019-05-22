62e3fe41c495d1b6318784f40a7c7f928ba6db50
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2019-05-22 18:34:53 -0300

    remove spark installation on windows

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,8 +3,16 @@ library(testthat)
 library(variantspark)
 library(sparklyr)
 
-spark_install(version = "2.4.0")
-spark_install(version = "2.3.2")
-spark_install(version = "2.2.1")
+if (identical(Sys.getenv("NOT_CRAN"), "true")) {
+  # enforce all configuration settings are described
+  options(sparklyr.test.enforce.config = TRUE)
+}
 
-test_check("variantspark")
+if (!grepl("WINDOWS", Sys.getenv("SystemRoot"))){
+  spark_install(version = "2.4.0")
+  spark_install(version = "2.3.2")
+  spark_install(version = "2.2.1")
+  test_check("variantspark")
+}
+
+
